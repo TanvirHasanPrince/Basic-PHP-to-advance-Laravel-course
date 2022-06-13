455f99060e926977af7e79d9ef90b4e9421cb016
Tanvir Hasan Prince <t.hasan.prince@gmail.com>
2022-06-13 14:18:02 +0600

    Class 17 Assignment

---
+++ 1244_Class_17/banner_List.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head.php'; ?>
+
+<body>
+
+	<?php include 'includes/mainNav.php'; ?>
+
+
+	<!-- Page container -->
+	<div class="page-container">
+
+		<!-- Page content -->
+		<div class="page-content">
+
+			<!-- Main sidebar -->
+			<div class="sidebar sidebar-main">
+				<div class="sidebar-content">
+
+					<!-- User menu -->
+					<div class="sidebar-user">
+						<div class="category-content">
+							<div class="media">
+								<a href="#" class="media-left"><img src="assets/images/placeholder.jpg" class="img-circle img-sm" alt=""></a>
+								<div class="media-body">
+									<span class="media-heading text-semibold">Victoria Baker</span>
+									<div class="text-size-mini text-muted">
+										<i class="icon-pin text-size-small"></i> &nbsp;Santa Ana, CA
+									</div>
+								</div>
+
+								<div class="media-right media-middle">
+									<ul class="icons-list">
+										<li>
+											<a href="#"><i class="icon-cog3"></i></a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<!-- /user menu -->
+
+
+					<?php include 'includes/navigation.php'; ?>
+
+				</div>
+			</div>
+			<!-- /main sidebar -->
+
+
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<!-- Page header -->
+				<div class="page-header">
+
+
+					<div class="breadcrumb-line">
+						<ul class="breadcrumb">
+
+							<li>
+								<icon= href="#"> <i class="icon-image-compare position-left"></i>Banner</a>
+							</li>
+							<li class="active">List</li>
+						</ul>
+
+						<ul class="breadcrumb-elements">
+							<li><a href="#"><i class="icon-comment-discussion position-left"></i> Support</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+									<i class="icon-gear position-left"></i>
+									Settings
+									<span class="caret"></span>
+								</a>
+
+								<ul class="dropdown-menu dropdown-menu-right">
+									<li><a href="#"><i class="icon-user-lock"></i> Account security</a></li>
+									<li><a href="#"><i class="icon-statistics"></i> Analytics</a></li>
+									<li><a href="#"><i class="icon-accessibility"></i> Accessibility</a></li>
+									<li class="divider"></li>
+									<li><a href="#"><i class="icon-gear"></i> All settings</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- /page header -->
+
+
+				<!-- Content area -->
+				<div class="content">
+
+					<!-- Basic datatable -->
+					<div class="panel panel-flat">
+						<div class="panel-heading">
+							<h5 class="panel-title">Banner List
+
+							</h5>
+							<div class="heading-elements">
+								<ul class="icons-list">
+									<li style='margin-right: 0px;'><a href="banner_create.php" class="btn btn-primary add-new">Add new</a></li>
+
+									<!-- <li><a data-action="collapse"></a></li>
+			                		<li><a data-action="reload"></a></li>
+			                		<li><a data-action="close"></a></li> -->
+								</ul>
+							</div>
+						</div>
+
+						<div class="panel-body">
+							<table class="table table-bordered datatable-basic">
+								<thead>
+									<tr>
+										<th width="5%">SL.</th>
+										<th width="20%">First Name</th>
+										<th width="20%">Last Name</th>
+										<th width="20%">Job Title</th>
+										<th width="20%">DOB</th>
+										<th width="5%">Status</th>
+										<th width="10%" class="text-center">Actions</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>1</td>
+										<td>Marth</td>
+										<td><a href="#">Enright</a></td>
+										<td>Traffic Court Referee</td>
+										<td>22 Jun 1972</td>
+										<td><span class="label label-success">Active</span></td>
+										<td class="text-center">
+											<a href="banner_update.php">
+												<i class=" icon-pencil4"></i>
+											</a>
+
+											<a href="#">
+												<i class="  icon-trash"></i>
+											</a>
+
+										</td>
+									</tr>
+
+								</tbody>
+							</table>
+
+						</div>
+
+
+					</div>
+					<!-- /basic datatable -->
+
+
+
+
+
+
+
+					<!-- Footer -->
+					<div class="footer text-muted">
+						&copy; 2015. <a href="#">Limitless Web App Kit</a> by <a href="http://themeforest.net/user/Kopyov" target="_blank">Eugene Kopyov</a>
+					</div>
+					<!-- /footer -->
+
+				</div>
+				<!-- /content area -->
+
+			</div>
+			<!-- /main content -->
+
+		</div>
+		<!-- /page content -->
+
+	</div>
+	<!-- /page container -->
+
+	<?php include 'includes/script.php'; ?>
+</body>
+
+</html>
